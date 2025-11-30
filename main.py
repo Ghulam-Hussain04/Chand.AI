337465e9f9e2eda70ec8495131f533a4bb0d094a
hamzadev22 <hamza.hussain@benchmatrix.ca>
2025-11-30 17:05:42 +0500

    LLM Pipeline Start

---
+++ main.py
@@ -0,0 +1,19 @@
+from fastapi import FastAPI , UploadFile , File , HTTPException
+from fastapi.middleware.cors import CORSMiddleware
+
+app = FastAPI()
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=["*"],
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
+
+@app.get("/upload_and_vectorize")
+async def upload_and_vectorize():
+    try:
+        return {"detail": f"✅ Successfully vectorized "}
+    except Exception as e:
+        return {"detail": "sorry something went wrong", "error": str(e)}
